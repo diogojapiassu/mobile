72890f1084dd58fe33a95e7b42b8d1ba2d7ce9b7
diogojapiassu <diogojapiassu@gmail.com>
2014-05-13 18:49:49 -0300

    Formatação de código.

--- Mobile/src/br/ufg/inf/fs/android/persist/PersistenceHelper.java
+++ Mobile/src/br/ufg/inf/fs/android/persist/PersistenceHelper.java
@@ -3,10 +3,11 @@ package br.ufg.inf.fs.android.persist;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.view.View.OnCreateContextMenuListener;
 
 public class PersistenceHelper extends SQLiteOpenHelper {
  
-    public static final String NOME_BANCO =  "NotificaUniversidade";
+    public static final String NOME_BANCO = "NotificaUniversidade";
     public static final int VERSAO =  1;
      
     private static PersistenceHelper instance;
@@ -16,8 +17,9 @@ public class PersistenceHelper extends SQLiteOpenHelper {
     }
      
     public static PersistenceHelper getInstance(Context context) {
-        if(instance == null)
-            instance = new PersistenceHelper(context);
+        if(instance == null){
+        	instance = new PersistenceHelper(context);
+        }
          
         return instance;
     }
