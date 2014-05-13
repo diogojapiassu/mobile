66765d3535fa04a3990b4f0390e8ea681e37404b
diogojapiassu <diogojapiassu@gmail.com>
2014-05-13 18:15:09 -0300

    Classes para auxiliar a persistência

--- Mobile/gen/br/ufg/inf/fs/android/a01/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package br.ufg.inf.fs.android.a01;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
---
+++ Mobile/src/br/ufg/inf/fs/android/persist/UsuarioDAO.java
@@ -0,0 +1,123 @@
+package br.ufg.inf.fs.android.persist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class UsuarioDAO {
+	 
+	 
+    public static final String NOME_TABELA = "Usuario";
+    public static final String COLUNA_ID = "id";
+    public static final String COLUNA_NOME = "nome";
+    public static final String COLUNA_LOGIN = "login";
+    public static final String COLUNA_SENHA = "senha";
+ 
+ 
+    public static final String SCRIPT_CRIACAO_TABELA_USUARIOS = "CREATE TABLE " + NOME_TABELA + "("
+            + COLUNA_ID + " INTEGER PRIMARY KEY," + COLUNA_NOME + " TEXT," + COLUNA_SENHA + " TEXT,"
+            + COLUNA_LOGIN + " TEXT" + ")";
+ 
+    public static final String SCRIPT_DELECAO_TABELA =  "DROP TABLE IF EXISTS " + NOME_TABELA;
+ 
+ 
+    private SQLiteDatabase dataBase = null;
+ 
+ 
+    private static UsuarioDAO instance;
+     
+    public static UsuarioDAO getInstance(Context context) {
+        if(instance == null)
+            instance = new UsuarioDAO(context);
+        return instance;
+    }
+ 
+    private UsuarioDAO(Context context) {
+        PersistenceHelper persistenceHelper = PersistenceHelper.getInstance(context);
+        dataBase = persistenceHelper.getWritableDatabase();
+    }
+ 
+    public void salvar(Usuario usuario) {
+        ContentValues values = gerarContentValeuesUsuario(usuario);
+        dataBase.insert(NOME_TABELA, null, values);
+    }
+ 
+    public List<Usuario> recuperarTodos() {
+        String queryReturnAll = "SELECT * FROM " + NOME_TABELA;
+        Cursor cursor = dataBase.rawQuery(queryReturnAll, null);
+        List<Usuario> usuarios = construirUsuarioPorCursor(cursor);
+ 
+        return usuarios;
+    }
+ 
+    public void deletar(Usuario usuario) {
+ 
+        String[] valoresParaSubstituir = {
+                String.valueOf(usuario.getId())
+        };
+ 
+        dataBase.delete(NOME_TABELA, COLUNA_ID + " =  ?", valoresParaSubstituir);
+    }
+ 
+    public void editar(Usuario usuario) {
+        ContentValues valores = gerarContentValeuesUsuario(usuario);
+ 
+        String[] valoresParaSubstituir = {
+                String.valueOf(usuario.getId())
+        };
+ 
+        dataBase.update(NOME_TABELA, valores, COLUNA_ID + " = ?", valoresParaSubstituir);
+    }
+ 
+    public void fecharConexao() {
+        if(dataBase != null && dataBase.isOpen())
+            dataBase.close(); 
+    }
+ 
+ 
+    private List<Usuario> construirUsuarioPorCursor(Cursor cursor) {
+        List<Usuario> usuarios = new ArrayList<Usuario>();
+        if(cursor == null)
+            return usuarios;
+         
+        try {
+ 
+            if (cursor.moveToFirst()) {
+                do {
+ 
+                    int indexID = cursor.getColumnIndex(COLUNA_ID);
+                    int indexNome = cursor.getColumnIndex(COLUNA_NOME);
+                    int indexLogin = cursor.getColumnIndex(COLUNA_LOGIN);
+                    int indexSenha = cursor.getColumnIndex(COLUNA_SENHA);
+ 
+                    int id = cursor.getInt(indexID);
+                    String nome = cursor.getString(indexNome);
+                    String login = cursor.getString(indexLogin);
+                    String senha = cursor.getString(indexSenha);
+ 
+                    Usuario usuario = new Usuario(id, nome, login, senha);
+ 
+                    usuarios.add(usuario);
+ 
+                } while (cursor.moveToNext());
+            }
+             
+        } finally {
+            cursor.close();
+        }
+        return usuarios;
+    }
+ 
+    private ContentValues gerarContentValeuesUsuario(Usuario usuario) {
+        ContentValues values = new ContentValues();
+        values.put(COLUNA_NOME, usuario.getNome());
+        values.put(COLUNA_LOGIN, usuario.getLogin());
+        values.put(COLUNA_SENHA, usuario.getSenha());
+ 
+        return values;
+    }
+}
